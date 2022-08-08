cc0335c34657e035c6a3300ed14dec61133b4094
re-nutsugah <103048566+re-nutsugah@users.noreply.github.com>
2022-08-08 18:25:35 +0000

    Update sum_even.py
    Update sum of even numbers to average of even numbers

--- sum_even.py
+++ sum_even.py
@@ -1,14 +1,21 @@
-def sum_even(max):
-    sum = 0
-    for i in range(1, max+1):
+def ave_even(max):
+    even_num = []
+    res = 0
+    for i in range(1, max):
         if i % 2 == 0:
-            sum += i
-    print(sum)
+            even_num.append(i)
+    for j in even_num:
+    	res += j
+    try:
+    	average = res//len(even_num)
+    except ZeroDivisionError:
+    	average = 0
+    print(f"The average of even numbers between 1 and {max} is {average}")
 
 
-sum_even(1)
-sum_even(6)
-sum_even(10)
-sum_even(120)
-sum_even(1000)
-sum_even(10000)
+ave_even(2)
+ave_even(6)
+ave_even(10)
+ave_even(120)
+ave_even(1000)
+ave_even(10000)
